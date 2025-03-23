ff7e26c8e7eced5939b74be4cc89aecd008d4af7
HamzaAkb <hamzawakbar@gmail.com>
2025-03-24 03:09:59 +0500

    refactor: optimize string concatenation in 606. Construct String from Binary Tree

--- trees/06_construct_string_from_binary_tree.go
+++ trees/06_construct_string_from_binary_tree.go
@@ -14,13 +14,14 @@ func tree2str(root *TreeNode) string {
     result := strconv.Itoa(root.Val)
 
     if root.Left != nil {
-        result = fmt.Sprintf("%s(%s)", result, tree2str(root.Left))
+        result += "(" + tree2str(root.Left) + ")"
     } else if  root.Right != nil {
-        result = fmt.Sprintf("%s()", result)
+        result += "()"
     }
 
     if root.Right != nil {
-        result = fmt.Sprintf("%s(%s)", result, tree2str(root.Right))
+        result += "(" + tree2str(root.Right) + ")"
+
     }
 
     return result
